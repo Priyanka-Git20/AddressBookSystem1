3d9da44f8630125f9e97a0743f589d73f0c086a9
priyanka <priyanka.chavan1408@gmail.com>
2022-04-15 00:05:27 +0530

    [Priyanka]refactor:Use main function.

--- addressBookSystem.py
+++ addressBookSystem.py
@@ -6,6 +6,7 @@
     @Title :Ability to edit the contact in address book.
 '''
 
+
 class AddressBook:
     contact = {}
     contact_length = len(contact)
@@ -29,28 +30,27 @@ class AddressBook:
         self.phoneNumber = phoneNumber
         self.email = email
 
-    def addContacts(self):
+    def addContacts(self, firstName, lastName, address, city, state, zip, phoneNumber, emailId):
         """
         Description:
             Taking the input from the user and add into the list .
         Parameter:
-            self is as parameter
+            person info like first name,last name, city,state,zip,phone number,emailId.
         Return:
-            Returning dictionary.
+            Returning the dictionary.
         """
         lst = []
-        lst.append(str(input("Enter the first name : ")))
-        lst.append(str(input("Enter the last name : ")))
-        lst.append(str(input("Enter the address : ")))
-        lst.append(str(input("Enter the city : ")))
-        lst.append(str(input("Enter the state : ")))
-        lst.append(int(input("Enter the zip : ")))
-        lst.append(int(input("Enter the phone number: ")))
-        lst.append(str(input("Enter the emailId : ")))
+        lst.append(firstName)
+        lst.append(lastName)
+        lst.append(address)
+        lst.append(city)
+        lst.append(state)
+        lst.append(zip)
+        lst.append(phoneNumber)
+        lst.append(emailId)
         contact_length = len(AddressBook.contact)
         AddressBook.contact[contact_length] = lst
         AddressBook.contact_length += 1
-
         return AddressBook.contact
 
     def displayContact(self):
@@ -60,18 +60,18 @@ class AddressBook:
         Parameter:
             self is as parameter
         Return:
-            Returning nothing but printing the dictionary.
+            Returning nothing but printing the list.
         """
         print(AddressBook.contact)
 
-    def editContact(self):
+    def editContact(self,firstName, lastName, address, city, state, zip, phoneNumber, emailId):
         """
         Description:
             First check the input taken from user is in list and then modify the list .
         Parameter:
-            self is as parameter
+             person info like first name,last name, city,state,zip,phone number,emailId.
         Return:
-            Returning dictionary.
+            Returning the name which we check is it in list or not and dictionary
         """
 
         checkName = input("Enter the first name to check is it in contact or not")
@@ -79,13 +79,13 @@ class AddressBook:
             if checkName == AddressBook.contact[i][0]:
                 print("Exists in list")
                 break
-            else:
-                print("Not in list")
-                exit()
+        else:
+            print("Not in list")
+            exit()
 
         while True:
             option = int(input(
-                '1. To modify First name, 2. To modify last name, 3. To modify address, 4. To modify city, 5. To modify state, 6. To modify zip, 7.To modify email, 8. To modify phone number: '))
+                '1. To modify First name, 2. To modify last name, 3. To modify address, 4. To modify city, 5. To modify state, 6. To modify zip, 7.To modify email, 8. To modify phone number'))
             if option == 1:
                 firstName = str(input("Enter the first name : "))
                 del AddressBook.contact[i][0]
@@ -120,7 +120,8 @@ class AddressBook:
                 AddressBook.contact[i].insert(7, emailId)
             else:
                 break
-        return checkName, AddressBook.contact
+
+        return checkName, AddressBook.contact,AddressBook.contact_length
 
     def deleteContacts(self):
         """
@@ -141,35 +142,31 @@ class AddressBook:
                 print("Not in list")
         return AddressBook.contact
 
-    def menu(self):
-        """
-        Description:
-            this menu function in this we have shown total menu to the user for user interface and we are calling functions with user requirement
-        Parameter:
-            self is as parameter
-        Return:
-            Returning nothing just doing operations
-        """
-
-        print(
-            'Enter 1. To Add Contacts 2. For display a Contact  3.To edit the contacts 4.To delete the contact 5.To Exit')
-        while True:
-            choice = int(input('Enter your choice: '))
-            if choice == 1:
-                self.addContacts()
-            elif choice == 2:
-                self.displayContact()
-            elif choice == 3:
-                self.editContact()
-            elif choice == 4:
-                self.deleteContacts()
-            elif choice == 5:
-                exit()
-            else:
-                print('Invalid Option. Try Again!')
-
 
 if __name__ == '__main__':
     print("Welcome to the address book system.")
     myBook = AddressBook()
-    myBook.menu()
+    print(
+        'Enter 1. To Add Contacts 2. For display a Contact  3.To Exit')
+    while True:
+        choice = int(input('Enter your choice: '))
+        if choice == 1:
+            firstName = str(input("Enter the first name :\n"))
+            lastName = str(input("Enter the last name :\n"))
+            address = str(input("Enter the address :\n"))
+            city = str(input("Enter the city :\n"))
+            state = str(input("Enter the state :\n"))
+            zip = input("Enter the zip :\n")
+            phoneNumber = input("Enter the phone number :\n")
+            emailId = str(input("Enter the email address :\n"))
+            myBook.addContacts(firstName,lastName,address,city,state,zip,phoneNumber,emailId)
+        elif choice == 2:
+            myBook.displayContact()
+        elif choice == 3:
+            myBook.editContact(firstName=1, lastName=2, address=3,city= 4, state=5, zip=6, phoneNumber=7, emailId=8)
+        elif choice == 4:
+            exit()
+        else:
+            print('Invalid Option. Try Again!')
+
+
--- test_addressBookSystem.py
+++ test_addressBookSystem.py
@@ -1,23 +1,24 @@
 import unittest
 from addressBookSystem import AddressBook
+book = AddressBook()
 
 
 class TestAddressBook(unittest.TestCase):
 
     def test_addContacts(self):
-        book = AddressBook()
-        book.addContacts()
-        self.assertEqual(len(AddressBook.contact),1)
+        book.addContacts("Siya", "Salunkhe", "Satara", "Satara", "Maharastra", "415507", "7066647330",
+                         "priyanka.chavan1408@gmail.com")
+        self.assertEqual(len(book.contact), 1)
+        self.assertTrue(len(book.contact), 1)
 
-    # def test_editContact(self):
-    #     book = AddressBook()
-    #     book.editContact()
-    #     self.assertEqual(AddressBook.contact[0][0],"piya")
+    def test_editContact(self):
+        book.editContact("Siya", "Salunkhe", "Satara", "Satara", "Maharastra", "415507", "7066647330",
+                         "priyanka.chavan1408@gmail.com")
+        self.assertEqual(book.contact[0][0], "piya")
 
     def test_deleteContacts(self):
-        book = AddressBook()
         book.deleteContacts()
-        self.assertEqual(len(AddressBook.contact),0)
+        self.assertGreaterEqual(len(AddressBook.contact),0)
 
 
 if __name__ == '__main__':
