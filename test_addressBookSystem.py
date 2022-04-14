cfddcf6d92477609a11638d5ec151dac4322ddc7
priyanka <priyanka.chavan1408@gmail.com>
2022-04-14 15:55:09 +0530

    [Priyanka]refactor:Use main function.

--- test_addressBookSystem.py
+++ test_addressBookSystem.py
@@ -1,10 +1,17 @@
 import unittest
 from addressBookSystem import AddressBook
+book = AddressBook()
 
 
 class TestAddressBook(unittest.TestCase):
-
     def test_addContacts(self):
         book = AddressBook()
-        book.addContacts()
-        self.assertEqual(len(AddressBook.contact),1)
+        book.addContacts("Siya","Salunkhe","Satara","Satara","Maharastra","415507","7066647330","priyanka.chavan1408@gmail.com")
+        self.assertEqual(len(book.contact),1)
+        self.assertTrue(len(book.contact), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
